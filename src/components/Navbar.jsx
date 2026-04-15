09eb69f09849ed3e273012f195ef11d3940c9ad9
ALONSO JUAREZ <iajuarez@segulagrp.es>
2026-04-15 12:26:33 +0200

    reorden del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
         <div className="nav-tabs-wrapper">
           <a href="#inicio" className="tab-item active"><span>Inicio</span></a>
           <a href="#modulos" className="tab-item"><span>Módulos</span></a>
-          <a href="#proyecto" className="tab-item"><span>Proyecto</span></a>
           <a href="#servicios" className="tab-item"><span>Servicios</span></a>
+          <a href="#proyecto" className="tab-item"><span>Proyecto</span></a>
           <a href="#contacto" className="tab-item"><span>Contacto</span></a>
         </div>
       </nav>
